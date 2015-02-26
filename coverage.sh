88b93c13f8d3761a61a1ced8b8ff27c02a86f70d
Hal Blackburn <hal.blackburn@gmail.com>
2015-02-26 22:54:22 +0000

    Improve coverage reporting in tox/coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -1,9 +1,19 @@
 # Combine coverage of Python 2 and Python 3 runs to catch 2/3 specific code
 set -e
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
 echo "Combining coverage of Python 2 and 3 test runs:"
 echo "==============================================="
+
 coverage erase
-tox -e py27,py34
+# the coverage_combined factor uses the -p coverage run switch to save coverage
+# from different runs in different coverage files.
+tox -e "{py27,py34}-coverage_combined"
 coverage combine
 coverage html
+
+echo
+echo "Combined coverage of Python 2 and 3 test runs:"
+echo "=============================================="
+echo
 coverage report
