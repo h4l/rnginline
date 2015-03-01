f25facdf608ea5c4975a7e8ba6053d05a9d9d0f0
Hal Blackburn <hal.blackburn@gmail.com>
2015-03-01 11:20:24 +0000

    Increase test coverage of relaxnginline.uri

--- relaxnginline/test/test_uri.py
+++ relaxnginline/test/test_uri.py
@@ -25,6 +25,13 @@ def test_urllib_parsing_is_not_that_great():
         "", "", "f%69le:///foo", "", "")  # WTF...
 
 
+def test_urllib_urlunsplit_adds_empty_netloc_on_file_urls():
+    parts = ("file", "", "/foo/bar", "", "")
+    assert (urllib.parse.urlunsplit(parts) == "file:///foo/bar")
+
+    assert uri.recombine(parts) == "file:/foo/bar"
+
+
 # Test here are from RFC 3986 section 5.4:
 # http://tools.ietf.org/html/rfc3986#section-5.4
 
@@ -79,7 +86,24 @@ BASE = ("http://a/b/c/d;p?q",)
     BASE + ("g?y/./x", "http://a/b/c/g?y/./x"),
     BASE + ("g?y/../x", "http://a/b/c/g?y/../x"),
     BASE + ("g#s/./x", "http://a/b/c/g#s/./x"),
-    BASE + ("g#s/../x", "http://a/b/c/g#s/../x")
+    BASE + ("g#s/../x", "http://a/b/c/g#s/../x"),
+
+    # Extras not from the RFC:
+
+    # Resolving relative path against abs URI w/ no path
+    ("x://blah", "rel/path", "x://blah/rel/path"),
+    ("x://blah", "./rel/path", "x://blah/rel/path"),
+
+    ("x://blah", ".", "x://blah/"),
+    ("x://blah", "..", "x://blah/"),
+
+    ("z:relpath", ".", "z:"),
+    ("z:relpath", "..", "z:"),
+
+    ("x://blah", "y:../rel/path", "y:rel/path"),
+    ("x://blah", "y:./rel/path", "y:rel/path"),
+
+
 ])
 def test_resolve(base, reference, target):
     assert uri.resolve(base, reference) == target
@@ -94,9 +118,95 @@ def test_strict_resolve(base, reference, target, strict):
     assert uri.resolve(base, reference, strict=strict) == target
 
 
-def test_recombine_rejects_relative_paths_with_netloc():
-    with pytest.raises(uri.UriSyntaxError):
-        # This would produce "foorel/path" if an error was not raised.
-        # We could do what urllib does and insert a leading /, but this doesn't
-        # sit well with PEP 20 IMO.
-        uri.recombine(("", "foo", "rel/path", "", ""))
+@pytest.mark.parametrize("base,raises", [
+    ("x:/blah", False),
+    ("x:/blah blah", True),  # contains space
+    ("", True),
+    ("/a/path", True),
+    ("//host/a/path", True),
+])
+def test_resolve_base_must_be_uri(base, raises):
+    if raises is True:
+        with pytest.raises(ValueError):
+            uri.resolve(base, "")
+    else:
+        uri.resolve(base, "")
+
+
+@pytest.mark.parametrize("ref,raises", [
+    ("x:/blah", False),
+    ("x:/blah blah", True),  # contains space
+    ("", False),
+    ("/a/path", False),
+    ("//host/a/path", False),
+])
+def test_resolve_reference_must_be_uri_ref(ref, raises):
+    if raises is True:
+        with pytest.raises(ValueError):
+            uri.resolve("x:/foo", ref)
+    else:
+        uri.resolve("x:/foo", ref)
+
+
+@pytest.mark.parametrize("parts,expected", [
+    # testcases are cartesian product of:
+    # [("", "a"), ("", "b"), ("", "c", "/c"), ("", "d=d"), ("", "e")]
+    (("", "", "", "", ""),         ""),
+    (("", "", "", "", "e"),        "#e"),
+    (("", "", "", "d=d", ""),      "?d=d"),
+    (("", "", "", "d=d", "e"),     "?d=d#e"),
+    (("", "", "c", "", ""),        "c"),
+    (("", "", "c", "", "e"),       "c#e"),
+    (("", "", "c", "d=d", ""),     "c?d=d"),
+    (("", "", "c", "d=d", "e"),    "c?d=d#e"),
+    (("", "", "/c", "", ""),       "/c"),
+    (("", "", "/c", "", "e"),      "/c#e"),
+    (("", "", "/c", "d=d", ""),    "/c?d=d"),
+    (("", "", "/c", "d=d", "e"),   "/c?d=d#e"),
+    (("", "b", "", "", ""),        "//b"),
+    (("", "b", "", "", "e"),       "//b#e"),
+    (("", "b", "", "d=d", ""),     "//b?d=d"),
+    (("", "b", "", "d=d", "e"),    "//b?d=d#e"),
+    (("", "b", "c", "", ""),       None),  # syntax error
+    (("", "b", "c", "", "e"),      None),
+    (("", "b", "c", "d=d", ""),    None),
+    (("", "b", "c", "d=d", "e"),   None),
+    (("", "b", "/c", "", ""),      "//b/c"),
+    (("", "b", "/c", "", "e"),     "//b/c#e"),
+    (("", "b", "/c", "d=d", ""),   "//b/c?d=d"),
+    (("", "b", "/c", "d=d", "e"),  "//b/c?d=d#e"),
+    (("a", "", "", "", ""),        "a:"),
+    (("a", "", "", "", "e"),       "a:#e"),
+    (("a", "", "", "d=d", ""),     "a:?d=d"),
+    (("a", "", "", "d=d", "e"),    "a:?d=d#e"),
+    (("a", "", "c", "", ""),       "a:c"),
+    (("a", "", "c", "", "e"),      "a:c#e"),
+    (("a", "", "c", "d=d", ""),    "a:c?d=d"),
+    (("a", "", "c", "d=d", "e"),   "a:c?d=d#e"),
+    (("a", "", "/c", "", ""),      "a:/c"),
+    (("a", "", "/c", "", "e"),     "a:/c#e"),
+    (("a", "", "/c", "d=d", ""),   "a:/c?d=d"),
+    (("a", "", "/c", "d=d", "e"),  "a:/c?d=d#e"),
+    (("a", "b", "", "", ""),       "a://b"),
+    (("a", "b", "", "", "e"),      "a://b#e"),
+    (("a", "b", "", "d=d", ""),    "a://b?d=d"),
+    (("a", "b", "", "d=d", "e"),   "a://b?d=d#e"),
+    (("a", "b", "c", "", ""),      None),
+    (("a", "b", "c", "", "e"),     None),
+    (("a", "b", "c", "d=d", ""),   None),
+    (("a", "b", "c", "d=d", "e"),  None),
+    (("a", "b", "/c", "", ""),     "a://b/c"),
+    (("a", "b", "/c", "", "e"),    "a://b/c#e"),
+    (("a", "b", "/c", "d=d", ""),  "a://b/c?d=d"),
+    (("a", "b", "/c", "d=d", "e"), "a://b/c?d=d#e")
+])
+def test_recombine(parts, expected):
+    if expected is None:
+        assert parts[1] and not parts[2].startswith("/")
+        with pytest.raises(uri.UriSyntaxError):
+            # This would produce "bc" if an error was not raised.
+            # We could do what urllib does and insert a leading /, but this
+            # doesn't sit well with PEP 20 IMO.
+            uri.recombine(parts)
+    else:
+        assert uri.recombine(parts) == expected
